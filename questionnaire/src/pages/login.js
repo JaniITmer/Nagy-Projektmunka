9cdd97740556bf9e657f6c1e554c33fd703293b4
Juhász Gergő <123juhasz.gergo123@gmail.com>
2023-11-14 21:27:57 +0100

    login oldal séma

--- questionnaire/src/pages/login.js
+++ questionnaire/src/pages/login.js
@@ -1,3 +1,20 @@
 export default function Login() {
-    return <h1>Bejelentkezés</h1>
+    return (
+        <div className="login">
+            <form>            
+            <h1>Bejelentkezés</h1>
+            <div>
+                <label htmlFor="username">Felhasználónév</label>
+                <input type="text" placeholder="Felhasználónév" className="control"/>
+            </div>
+            <div>
+                <label htmlFor="password">Jelszó</label>
+                <input type="password" placeholder="Jelszó" className="control"/>
+            </div>
+            <div>
+                <button className="button">Bejelentkezés</button>
+            </div>
+            </form>
+        </div>
+    )
 }

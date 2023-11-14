2f7f391fff6b3e9723d95ca826a0f345f982774e
buruadam <adam.buru@gmail.com>
2023-11-14 22:14:20 +0100

    Kérdőívtémák design

--- questionnaire/src/pages/Home.js
+++ questionnaire/src/pages/Home.js
@@ -6,25 +6,55 @@ export default function Home() {
         <h1>Kérdőív témák</h1>
         
         <div className="card">
+            <h2>Téma</h2>
             <div class="card-content">
-                <h2>Téma</h2>
+                <p>Kérdőív1</p>
+                <p>Kérdőív2</p>
+                <p>Kérdőív3</p>
             </div>
         </div>
         <div className="card">
+            <h2>Téma</h2>
             <div class="card-content">
-                <h2>Téma</h2>
+                <p>Kérdőív1</p>
+                <p>Kérdőív2</p>
+                <p>Kérdőív3</p>
             </div>
         </div>
         <div className="card">
+            <h2>Téma</h2>
             <div class="card-content">
-                <h2>Téma</h2>
+                <p>Kérdőív1</p>
+                <p>Kérdőív2</p>
+                <p>Kérdőív3</p>
             </div>
         </div>
         <div className="card">
+            <h2>Téma</h2>
             <div class="card-content">
-                <h2>Téma</h2>
+                <p>Kérdőív1</p>
+                <p>Kérdőív2</p>
+                <p>Kérdőív3</p>
             </div>
         </div>
+        <div className="card">
+            <h2>Téma</h2>
+            <div class="card-content">
+                <p>Kérdőív1</p>
+                <p>Kérdőív2</p>
+                <p>Kérdőív3</p>
+            </div>
+        </div>
+        <div className="card">
+            <h2>Téma</h2>
+            <div class="card-content">
+                <p>Kérdőív1</p>
+                <p>Kérdőív2</p>
+                <p>Kérdőív3</p>
+            </div>
+        </div>
+        
+        
 
       </div>
     )

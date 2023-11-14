0c871e1854405c4cbaca883c52db9820fe4e8fee
buruadam <adam.buru@gmail.com>
2023-11-14 22:29:49 +0100

    Főoldal elemek elrendezése

--- questionnaire/src/pages/Home.js
+++ questionnaire/src/pages/Home.js
@@ -2,8 +2,9 @@ import './Home.css';
 
 export default function Home() {
     return (
-      <div className="topics">
-        <h1>Kérdőív témák</h1>
+      <div class="main-page">
+        <div className="topics">
+        <h1>Elérhető kérdőív témák</h1>
         
         <div className="card">
             <h2>Téma</h2>
@@ -56,6 +57,11 @@ export default function Home() {
         
         
 
+        </div>
+        <div class="main-page-info">
+            <h1>Jelenleg vendég módban van!</h1>
+            <h1>A kérdőívek kitöltéséhez jelentkezzen be!</h1>
+        </div>
       </div>
     )
   }

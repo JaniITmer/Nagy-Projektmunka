4da2b474784bedc650c2b4813dd8c41fef7977a9
adamgbad <134494278+adamgbad@users.noreply.github.com>
2023-12-03 16:04:50 +0100

    Sql létrehozás

---
+++ questionnaire/db/MySQL.sql
@@ -0,0 +1,23 @@
+CREATE TABLE Felhasznalok (
+    FelhasznaloID INT PRIMARY KEY AUTO_INCREMENT,
+    FelhasznaloNev VARCHAR(50) NOT NULL,
+    Email VARCHAR(100) NOT NULL,
+    Jelszo VARCHAR(255) NOT NULL
+);
+CREATE TABLE Kerdoivek (
+    KerdoivID INT PRIMARY KEY AUTO_INCREMENT,
+    Cim VARCHAR(100) NOT NULL
+);
+CREATE TABLE Kerdesek (
+    KerdesID INT PRIMARY KEY AUTO_INCREMENT,
+    KerdoivID INT,
+    QuestionText TEXT NOT NULL,
+    FOREIGN KEY (SurveyID) REFERENCES Surveys(SurveyID)
+);
+CREATE TABLE Valaszok (
+    ValaszID INT PRIMARY KEY AUTO_INCREMENT,
+    KerdesID INT,
+    ValaszSzam TEXT NOT NULL,
+    FOREIGN KEY (QuestionID) REFERENCES Questions(QuestionID)
+);
+

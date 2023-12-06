7f6061a2a13a92a1997a7036bde7fa428d01e869
adamgbad <134494278+adamgbad@users.noreply.github.com>
2023-12-06 12:38:25 +0100

    MySql hibajavítás

--- questionnaire/db/MySQL.sql
+++ questionnaire/db/MySQL.sql
@@ -4,20 +4,30 @@ CREATE TABLE Felhasznalok (
     Email VARCHAR(100) NOT NULL,
     Jelszo VARCHAR(255) NOT NULL
 );
+
 CREATE TABLE Kerdoivek (
     KerdoivID INT PRIMARY KEY AUTO_INCREMENT,
     Cim VARCHAR(100) NOT NULL
 );
+
 CREATE TABLE Kerdesek (
     KerdesID INT PRIMARY KEY AUTO_INCREMENT,
     KerdoivID INT,
     QuestionText TEXT NOT NULL,
     FOREIGN KEY (KerdoivID) REFERENCES Kerdoivek(KerdoivID)
 );
+
 CREATE TABLE Valaszok (
     ValaszID INT PRIMARY KEY AUTO_INCREMENT,
     KerdesID INT,
     ValaszSzam TEXT NOT NULL,
-    FOREIGN KEY (ValaszID) REFERENCES Questions(ValaszID)
+    FOREIGN KEY (KerdesID) REFERENCES Kerdesek(KerdesID)
 );
 
+INSERT INTO Felhasznalok (FelhasznaloNev, Email, Jelszo) 
+VALUES ('admin', 'admin@admin.com', 'admin');
+
+INSERT INTO Felhasznalok (FelhasznaloID, FelhasznaloNev, Email, Jelszo) 
+VALUES 
+(13, 'user1', 'user@user', '123'),
+(14, 'user2', 'admin@admin', '444');

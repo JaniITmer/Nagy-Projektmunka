a012f84ab46ab47a828275843a166bc7a8d46702
buruadam <adam.buru@gmail.com>
2023-12-09 18:02:04 +0100

    Új adatbázis fájl feltöltése

--- questionnaire/db/questionnaire_db.sql
+++ questionnaire/db/questionnaire_db.sql
@@ -2,10 +2,10 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Nov 29, 2023 at 08:41 PM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+-- Gép: 127.0.0.1
+-- Létrehozás ideje: 2023. Dec 09. 18:00
+-- Kiszolgáló verziója: 10.4.28-MariaDB
+-- PHP verzió: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,13 +18,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `questionnaire_db`
+-- Adatbázis: `questionnaire_db`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `answers`
+-- Tábla szerkezet ehhez a táblához `answers`
 --
 
 CREATE TABLE `answers` (
@@ -40,19 +40,42 @@ CREATE TABLE `answers` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `questions`
+-- Tábla szerkezet ehhez a táblához `questionnaires`
 --
 
-CREATE TABLE `questions` (
-  `question_id` int(11) NOT NULL,
-  `statistics_id` int(11) NOT NULL,
-  `question` varchar(255) NOT NULL
+CREATE TABLE `questionnaires` (
+  `questionnaire_id` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `statistics`
+-- Tábla szerkezet ehhez a táblához `questions`
+--
+
+CREATE TABLE `questions` (
+  `question_id` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `question1` varchar(255) NOT NULL,
+  `question2` varchar(255) NOT NULL,
+  `question3` varchar(255) NOT NULL,
+  `question4` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- A tábla adatainak kiíratása `questions`
+--
+
+INSERT INTO `questions` (`question_id`, `title`, `question1`, `question2`, `question3`, `question4`) VALUES
+(2, 'asd', 'asd1', 'asd2', 'asd3', 'asd4'),
+(3, 'Állatok', 'Kérdés1', 'Kérd2', 'Kérd3', 'Kérd4');
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `statistics`
 --
 
 CREATE TABLE `statistics` (
@@ -63,7 +86,7 @@ CREATE TABLE `statistics` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `users`
+-- Tábla szerkezet ehhez a táblához `users`
 --
 
 CREATE TABLE `users` (
@@ -74,19 +97,21 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `users`
+-- A tábla adatainak kiíratása `users`
 --
 
 INSERT INTO `users` (`id`, `username`, `email`, `password`) VALUES
-(13, 'user1', 'user@user', '123'),
-(14, 'user2', 'admin@admin', '444');
+(14, 'user2', 'admin@admin', '444'),
+(16, 'test', 'test@test', '12345'),
+(33, 'user', 'user@user', 'Asd1234!'),
+(34, 'user', 'user@user1', 'Asd1234!');
 
 --
--- Indexes for dumped tables
+-- Indexek a kiírt táblákhoz
 --
 
 --
--- Indexes for table `answers`
+-- A tábla indexei `answers`
 --
 ALTER TABLE `answers`
   ADD PRIMARY KEY (`answer_id`),
@@ -94,56 +119,61 @@ ALTER TABLE `answers`
   ADD KEY `FK_USERS` (`user_id`);
 
 --
--- Indexes for table `questions`
+-- A tábla indexei `questionnaires`
 --
-ALTER TABLE `questions`
-  ADD PRIMARY KEY (`question_id`),
-  ADD KEY `FK_STATISTICS` (`statistics_id`);
+ALTER TABLE `questionnaires`
+  ADD PRIMARY KEY (`questionnaire_id`);
 
 --
--- Indexes for table `statistics`
+-- A tábla indexei `questions`
+--
+ALTER TABLE `questions`
+  ADD PRIMARY KEY (`question_id`);
+
+--
+-- A tábla indexei `statistics`
 --
 ALTER TABLE `statistics`
   ADD PRIMARY KEY (`statistic_id`);
 
 --
--- Indexes for table `users`
+-- A tábla indexei `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- A kiírt táblák AUTO_INCREMENT értéke
 --
 
 --
--- AUTO_INCREMENT for table `questions`
+-- AUTO_INCREMENT a táblához `questionnaires`
+--
+ALTER TABLE `questionnaires`
+  MODIFY `questionnaire_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT a táblához `questions`
 --
 ALTER TABLE `questions`
-  MODIFY `question_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `question_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
--- AUTO_INCREMENT for table `users`
+-- AUTO_INCREMENT a táblához `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
 
 --
--- Constraints for dumped tables
+-- Megkötések a kiírt táblákhoz
 --
 
 --
--- Constraints for table `answers`
+-- Megkötések a táblához `answers`
 --
 ALTER TABLE `answers`
   ADD CONSTRAINT `FK_QUESTIONS` FOREIGN KEY (`question_id`) REFERENCES `questions` (`question_id`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `FK_USERS` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Constraints for table `questions`
---
-ALTER TABLE `questions`
-  ADD CONSTRAINT `FK_STATISTICS` FOREIGN KEY (`statistics_id`) REFERENCES `statistics` (`statistic_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;

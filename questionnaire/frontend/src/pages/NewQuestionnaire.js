1a78d9fb5782a42ea3bb8a966cb376ccbd4811b1
GerusJ <123juhasz.gergo123@gmail.com>
2023-12-10 23:23:54 +0100

    újkérdőív design

--- questionnaire/frontend/src/pages/NewQuestionnaire.js
+++ questionnaire/frontend/src/pages/NewQuestionnaire.js
@@ -50,83 +50,83 @@ export default function NewQuestionnaire() {
     return (
         <div className="main-page">
       <div className="topics">
-        <h1>Új kérdőív hozzáadása</h1>
+        <h1 className="newQues-H1">Új kérdőív hozzáadása</h1>
         
         <form className="inputs" action="" onSubmit={handleSubmit}>
                 <div className="title">
-                            <label className="form__label" htmlFor="title">Cím</label>
-                            <input  type="text" id="title"  className="form__input" placeholder="cím" 
+                            <label className="newQues-label" htmlFor="title">Cím</label>
+                            <input  type="text" id="title"  className="newQues-inputH1" placeholder="cím" 
                             name="title" onChange={handleInput}/>
                         </div>
                 <div className="newQues-div2">
                     <div className="question1">
-                            <label className="form__label" htmlFor="question1">Kérdés1</label>
-                            <input  type="text" id="question1" className="form__input" placeholder="kérdés1" 
+                            <label className="newQues-label" htmlFor="question1">Kérdés1</label>
+                            <input  type="text" id="question1" className="newQues-inputQues" placeholder="kérdés1" 
                             name="question1" onChange={handleInput}/>
                         </div>
                         <div className="option1_1">
-                            <label className="form__label" htmlFor="option1_1">Válasz1_1</label>
-                            <input  type="text" id="option1_1" className="form__input" placeholder="válasz1_1" 
+                            <label className="newQues-label" htmlFor="option1_1">Válasz1_1</label>
+                            <input  type="text" id="option1_1" className="newQues-inputAns" placeholder="válasz1_1" 
                             name="option1_1" onChange={handleInput}/>
                         </div>
                         <div className="option1_2">
-                            <label className="form__label" htmlFor="option1_2">Válasz1_2</label>
-                            <input  type="text" id="option1_2" className="form__input" placeholder="válasz1_2" 
+                            <label className="newQues-label" htmlFor="option1_2">Válasz1_2</label>
+                            <input  type="text" id="option1_2" className="newQues-inputAns" placeholder="válasz1_2" 
                             name="option1_2" onChange={handleInput}/>
                         </div>
                 </div>
                 <div className="newQues-div2">
                     <div className="question2">
-                            <label className="form__label" htmlFor="question2">Kérdés2</label>
-                            <input  type="text" id="question2" className="form__input" placeholder="kérdés2" 
+                            <label className="newQues-label" htmlFor="question2">Kérdés2</label>
+                            <input  type="text" id="question2" className="newQues-inputQues" placeholder="kérdés2" 
                             name="question2" onChange={handleInput}/>
                         </div>
                         <div className="option2_1">
-                            <label className="form__label" htmlFor="option2_1">Válasz2_1</label>
-                            <input  type="text" id="option2_1" className="form__input" placeholder="válasz2_1" 
+                            <label className="newQues-label" htmlFor="option2_1">Válasz2_1</label>
+                            <input  type="text" id="option2_1" className="newQues-inputAns" placeholder="válasz2_1" 
                             name="option2_1" onChange={handleInput}/>
                         </div>
                         <div className="option2_2">
-                            <label className="form__label" htmlFor="option2_2">Válasz2_2</label>
-                            <input  type="text" id="option2_2" className="form__input" placeholder="válasz2_2" 
+                            <label className="newQues-label" htmlFor="option2_2">Válasz2_2</label>
+                            <input  type="text" id="option2_2" className="newQues-inputAns" placeholder="válasz2_2" 
                             name="option2_2" onChange={handleInput}/>
                         </div>
                     </div>
                 <div className="newQues-div2">
                     <div className="question3">
-                            <label className="form__label" htmlFor="question3">Kérdés3</label>
-                            <input  type="text" id="question3" className="form__input" placeholder="kérdés3" 
+                            <label className="newQues-label" htmlFor="question3">Kérdés3</label>
+                            <input  type="text" id="question3" className="newQues-inputQues" placeholder="kérdés3" 
                             name="question3" onChange={handleInput}/>
                         </div>
                         <div className="option3_1">
-                            <label className="form__label" htmlFor="option3_1">Válasz3_1</label>
-                            <input  type="text" id="option3_1" className="form__input" placeholder="válasz3_1" 
+                            <label className="newQues-label" htmlFor="option3_1">Válasz3_1</label>
+                            <input  type="text" id="option3_1" className="newQues-inputAns" placeholder="válasz3_1" 
                             name="option3_1" onChange={handleInput}/>
                         </div>
                         <div className="option3_2">
-                            <label className="form__label" htmlFor="option3_2">Válasz3_2</label>
-                            <input  type="text" id="option3_2" className="form__input" placeholder="válasz3_2" 
+                            <label className="newQues-label" htmlFor="option3_2">Válasz3_2</label>
+                            <input  type="text" id="option3_2" className="newQues-inputAns" placeholder="válasz3_2" 
                             name="option3_2" onChange={handleInput}/>
                         </div>
                 </div>
                 <div className="newQues-div2">
                     <div className="question4">
-                            <label className="form__label" htmlFor="question4">Kérdés4</label>
-                            <input  type="text" id="question4" className="form__input" placeholder="kérdés4" 
+                            <label className="newQues-label" htmlFor="question4">Kérdés4</label>
+                            <input  type="text" id="question4" className="newQues-inputQues" placeholder="kérdés4" 
                             name="question4" onChange={handleInput}/>
                         </div>
                         <div className="option4_1">
-                            <label className="form__label" htmlFor="option4_1">Válasz4_1</label>
-                            <input  type="text" id="option4_1" className="form__input" placeholder="válasz4_1" 
+                            <label className="newQues-label" htmlFor="option4_1">Válasz4_1</label>
+                            <input  type="text" id="option4_1" className="newQues-inputAns" placeholder="válasz4_1" 
                             name="option4_1" onChange={handleInput}/>
                         </div>
                         <div className="option4_2">
-                            <label className="form__label" htmlFor="option4_2">Válasz4_2</label>
-                            <input  type="text" id="option4_2" className="form__input" placeholder="válasz4_2" 
+                            <label className="newQues-label" htmlFor="option4_2">Válasz4_2</label>
+                            <input  type="text" id="option4_2" className="newQues-inputAns" placeholder="válasz4_2" 
                             name="option4_2" onChange={handleInput}/>
                         </div>
                 </div>
-                    <button className='button' type='submit'>Kérdőív hozzáadása</button>
+                    <button className='newQues-button' type='submit'>Kérdőív hozzáadás</button>
                 </form>
 
       </div>
